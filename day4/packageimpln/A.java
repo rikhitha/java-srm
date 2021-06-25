020916d44355d43e7e6b6083ffb8c1abdbc43bb2
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-06-25 10:25:53 +0530

    Add files via upload

---
+++ day4/packageimpln/A.java
@@ -0,0 +1,14 @@
+package p1;
+
+import java.io.*;
+import java.util.*;
+
+public class A
+{
+public void display()
+{
+System.out.println("inside package p1");
+}
+ 
+
+}
---
+++ day4/packageimpln/B.java
@@ -0,0 +1,18 @@
+
+
+import p1.A;
+
+import java.io.*;
+import java.util.*;
+
+
+public class B
+{
+public static void main(String args[])
+{
+   A boj=new A();
+   boj.display();
+}
+ 
+
+}
