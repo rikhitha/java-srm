1e8790d44b0098a490bd3f98cebf4bed5433b594
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-06-29 14:23:18 +0530

    Update FileDetails.java

--- day7/FileDetails.java
+++ day7/FileDetails.java
@@ -19,6 +19,49 @@ class FileName
 	}
 }
 
+class CanRead
+{
+	public static void fileread(File f)
+	{
+		System.out.println("File is readable:: "+f.canRead());
+	}
+}
+
+class CanWrite
+{
+	public static void filewrite(File f)
+	{
+		System.out.println("File is writable::"+f.canWrite());	
+	}
+}
+
+class GetType
+
+{
+	public static void filetype(File f)
+	{
+            String fileName = f.toString();
+        
+		String extension="" ;
+		
+	    int index = fileName.lastIndexOf('.');
+	    if(index > 0) 
+	    {
+	      extension = fileName.substring(index + 1);
+	      System.out.println("File Type:: " + extension);
+	      
+	    }
+		
+	}
+}
+
+class GetLength
+{
+	public static void filelength(File f)
+	{
+		System.out.println("Length of the file:: "+f.length()+" bytes");
+	}
+}
 
 public class FileDetails 
 {
@@ -33,33 +76,21 @@ public class FileDetails
 		Filexists fe=new Filexists();
 		fe.exists(f);
 		
+		FileName fn=new FileName();
+		fn.filename(f);
 		
+		CanRead cr=new CanRead();
+		cr.fileread(f);
 		
-		String fileName = f.toString();
-        
-		String extension="" ;
+		CanWrite cw=new CanWrite();
+		cw.filewrite(f);
 		
-	    int index = fileName.lastIndexOf('.');
-	    if(index > 0) 
-	    {
-	      extension = fileName.substring(index + 1);
-	      
-	      
-	    }
-	    
+		GetType gt=new GetType();
+		gt.filetype(f);
 		
-		System.out.println("File name::"+f.getName());
-		
-		System.out.println("File is readable:: "+f.canRead());
-		
-		System.out.println("File is writable::"+f.canWrite());
-		
-		System.out.println("File Type:: " + extension);
-		
-		System.out.println("Length of the file:: "+f.length()+" bytes");
-		
-		
-		
+		GetLength gl=new GetLength();
+		gl.filelength(f);
+			
 
 	}
 
