3f3d3341764a8a50c087d2fcf83279736e6060f6
rikhitha <45781265+rikhitha@users.noreply.github.com>
2021-07-14 08:21:10 +0530

    Update Server.java

--- day20/Rmi/Server.java
+++ day20/Rmi/Server.java
@@ -7,14 +7,13 @@ public class Server extends ImplExample {
    public Server() {} 
    public static void main(String args[]) { 
       try { 
-         // Instantiating the implementation class 
+         
          ImplExample obj = new ImplExample(); 
     
-         // Exporting the object of implementation class  
-         // (here we are exporting the remote object to the stub) 
+         
          Hello stub = (Hello) UnicastRemoteObject.exportObject(obj, 0);  
          
-         // Binding the remote object (stub) in the registry 
+        
          Registry registry = LocateRegistry.getRegistry(); 
          
          registry.bind("Hello", stub);  
